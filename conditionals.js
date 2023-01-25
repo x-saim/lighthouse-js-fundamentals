303a301b9fe36729abb19785d2018f68ffa31316
Saim Amir <saimxamir@gmail.com>
2023-01-25 00:33:03 -0500

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,17 +1,12 @@
-const temperature = 22;
+//E.g 2
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+var temperature = -60;
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
 }
 
-console.log("Now you're ready to go outside!");
-/*
-What does temperature have to be for the program to tell us to pick a scarf?
-Answer: Any temperature below 0. E.g T = -1.
-What about for it to tell us that short sleeves are OK?
-Answer: Temperature above 15. E.g T = 16.
-*/
+//E.g 3
+var raining = false;
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
