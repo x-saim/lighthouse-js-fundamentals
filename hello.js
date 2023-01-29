6c3268327c9d38bc5481fc52bc67020be9a99919
Saim Amir <saimxamir@gmail.com>
2023-01-29 00:45:33 -0500

    isEven and sayHello Functions

--- hello.js
+++ hello.js
@@ -1,6 +1,5 @@
-const sayHello  = function (name) {
-  console.log("Hello, " + name);
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
 }
-sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
+const greeting = returnSayHello('John');
+console.log(greeting);
